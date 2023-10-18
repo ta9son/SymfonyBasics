1f3d7a2ad642f85a3c235fa8b8cc589bcac28bfc
Ta9son <machida.python3@gmail.com>
2023-10-19 08:07:16 +0900

    主な検証

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -4,22 +4,29 @@ namespace App\Entity;
 
 use App\Repository\PersonRepository;
 use Doctrine\ORM\Mapping as ORM;
+// エンティティに検証情報を追記
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PersonRepository::class)]
 class Person
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: "必ず値を入力してください")]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Assert\Email(message: "メールアドレスの形式で入力してください")]
+    #[Assert\NotBlank(message: "必ず値を入力してください")]
     private ?string $mail = null;
 
-    #[ORM\Column(nullable: true)]
+    #[ORM\Column(type: 'integer', nullable: true)]
+    #[Assert\Type(type: 'integer', message: "数値を入力してください")]
+    #[Assert\NotBlank(message: "必ず値を入力してください")]
     private ?int $age = null;
 
     public function getId(): ?int
